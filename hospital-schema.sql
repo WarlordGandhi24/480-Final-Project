09afe8382f8c994d46ca1126131afafeaedd3203
reevelood@gmail.com <reevelood@gmail.com>
2023-04-22 17:39:23 -0500

    foreign key changes

--- hospital-schema.sql
+++ hospital-schema.sql
@@ -4,30 +4,10 @@ CREATE DATABASE hospital;
 
 USE hospital;
 
-CREATE TABLE patient (
-  patient_id         INT            PRIMARY KEY   AUTO_INCREMENT,
-  name        		 VARCHAR(50)    NOT NULL,
-  address       	 VARCHAR(100)   NOT NULL,
-  phone_num          VARCHAR(15)    NOT NULL
-);
-
 CREATE TABLE room (
   room_num           INT            PRIMARY KEY,
   capacity        	 INT    	    NOT NULL,
   fee       	     INT   			NOT NULL
-);
-
-CREATE TABLE payment (
-  date         		 DATETIME       NOT NULL PRIMARY KEY,
-  amount             INT    		NOT NULL
-);
-
-CREATE TABLE health_record (
-  patient_id         INT            PRIMARY KEY   AUTO_INCREMENT,
-  disease            VARCHAR(30)    NOT NULL,
-  date       	 	 DATETIME       NOT NULL,
-  status          	 VARCHAR(15)    NOT NULL,
-  description		 TEXT
 );
 
 CREATE TABLE physician (
@@ -47,9 +27,39 @@ CREATE TABLE nurse (
   phone_num          VARCHAR(15)    NOT NULL
 );
 
+CREATE TABLE patient (
+  patient_id         INT            PRIMARY KEY   AUTO_INCREMENT,
+  name        		 VARCHAR(50)    NOT NULL,
+  address       	 VARCHAR(100)   NOT NULL,
+  phone_num          VARCHAR(15)    NOT NULL,
+  room_num			 INT			NOT NULL,
+  length_of_stay	 INT			NOT NULL,
+  physician_id		 INT			NOT NULL,
+  nurse_id			 INT			NOT NULL,
+  FOREIGN KEY (room_num) REFERENCES room(room_num),
+  FOREIGN KEY (physician_id) REFERENCES physician(physician_id),
+  FOREIGN KEY (nurse_id) REFERENCES nurse(nurse_id)
+);
+
+CREATE TABLE payment (
+  date         		 DATETIME       NOT NULL PRIMARY KEY,
+  amount             INT    		NOT NULL,
+  patient_id		 INT			NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
+);
+
+CREATE TABLE health_record (
+  patient_id         INT            PRIMARY KEY   AUTO_INCREMENT,
+  disease            VARCHAR(30)    NOT NULL,
+  date       	 	 DATETIME       NOT NULL,
+  status          	 VARCHAR(15)    NOT NULL,
+  description		 TEXT,
+  FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
+);
+
 CREATE TABLE medication (
-  patient_id         INT,
   name           	 VARCHAR(50)    PRIMARY KEY,
+  patient_id         INT,
   nurse_id       	 INT,
   FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
   FOREIGN KEY (nurse_id) REFERENCES nurse(nurse_id)
@@ -58,5 +68,11 @@ CREATE TABLE medication (
 CREATE TABLE instruction (
   code           	 INT            PRIMARY KEY,
   fee       	     INT   			NOT NULL,
-  description		 TEXT
+  description		 TEXT,
+  patient_id         INT			NOT NULL,
+  physician_id       INT			NOT NULL,
+  nurse_id           INT			NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+  FOREIGN KEY (physician_id) REFERENCES physician(physician_id),
+  FOREIGN KEY (nurse_id) REFERENCES nurse(nurse_id)
 );
