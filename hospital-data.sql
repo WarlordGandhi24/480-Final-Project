f2d2378bd9bb20d22a99cb2a1624322ef77fa7d0
reevelood@gmail.com <reevelood@gmail.com>
2023-04-26 07:51:05 -0500

    added hospital-data sql file

---
+++ hospital-data.sql
@@ -0,0 +1,17 @@
+insert into room values (101, 4, 500);
+insert into room values (102, 8, 300);
+insert into room values (103, 8, 300);
+insert into room values (104, 2, 800);
+insert into room values (105, 4, 500);
+
+insert into physician values (101, 'John Ham', 137, 'Surgery', '4040 Fake Street', '101-01-0111');
+insert into physician values (102, 'Jessica Worthy', 382, 'Eyes', '5555 Johnson Ave', '5252-03-5323');
+insert into physician values (103, 'Doctor Mario', 152, 'Terminal Illness', '4848 Mushroom Street', '2341-02-4323');
+insert into physician values (104, 'Zed Fyrestone', 467, 'Mental Illness', '2420 Border Boulevard', '5454-34-8415');
+insert into physician values (105, 'Ludwig Gehrmacht', 634, 'Surgery', '8049 Dustbowl Street', '111-11-1111');
+
+insert into nurse values (101, 'Eric Hanson', 332, '5848 Fake Street', '924-13-5439');
+insert into nurse values (101, 'Melissa Kiergan', 424, '4212 Wendigo Avenue', '434-54-5455');
+insert into nurse values (101, 'Joy Aflora', 976, '8292 Pallet Road', '999-99-9999');
+insert into nurse values (101, 'Claire Heel', 754, '4129 Hallow Street', '653-63-2453');
+insert into nurse values (101, 'Alan Benson', 728, '5829 Cap Avenue', '131-43-5321');
