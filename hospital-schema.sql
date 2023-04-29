acb7e027c05afd9032af1352280e5e071622f106
Ivan Deng <112420160+ydeng32@users.noreply.github.com>
2023-04-28 21:28:36 -0500

    update table
    1. drop some of the type datetime to date
    2.  change type of medication amount from int to string to reflect in to like 500mg
    3. change type of nurse_execution status from int to strong to reflect success or failure
    4. fix nurse_execution code to instruction(code)

--- hospital-schema.sql
+++ hospital-schema.sql
@@ -41,7 +41,7 @@ CREATE TABLE instruction (
   code           	 VARCHAR(15)            PRIMARY KEY,
   fee       	     INT   			NOT NULL,
   description		 TEXT,
-  order_date		 DATETIME		NOT NULL,
+  order_date		 DATE		NOT NULL,
   patient_id         INT			NOT NULL,
   physician_id       INT			NOT NULL,
   FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
@@ -49,7 +49,7 @@ CREATE TABLE instruction (
 );
 
 CREATE TABLE monitor (
-	duration	    INT			   NOT NULL,
+	duration	    VARCHAR(20)			   NOT NULL,
     physician_id		 INT			NOT NULL,
     patient_id		 INT			NOT NULL,
     FOREIGN KEY (physician_id) REFERENCES physician(physician_id),
@@ -57,7 +57,7 @@ CREATE TABLE monitor (
 );
 
 CREATE TABLE payment (
-  date         		 DATETIME       NOT NULL,
+  date         		 DATE       NOT NULL,
   amount             INT    		NOT NULL,
   patient_id		 INT			NOT NULL,
   PRIMARY KEY (date,patient_id),
@@ -67,31 +67,33 @@ CREATE TABLE payment (
 CREATE TABLE health_record (
   patient_id         INT            PRIMARY KEY   AUTO_INCREMENT,
   disease            VARCHAR(30)    NOT NULL,
-  date       	 	 DATETIME       NOT NULL,
+  date       	 	 DATE       NOT NULL,
   status          	 VARCHAR(15)    NOT NULL,
   description		 TEXT,
   FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
 );
 
+
+
 CREATE TABLE medication (
   name           	 VARCHAR(50)    PRIMARY KEY,
-  amount				 INT,
+  amount		     VARCHAR(15),
   patient_id         INT,
-  nurse_id       	 INT,
-  FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
-  FOREIGN KEY (nurse_id) REFERENCES nurse(nurse_id)
+  FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
 );
 
+
+
 CREATE TABLE nurse_execution (
-	status		INT,
+	status		VARCHAR(15),
     nurse_id    INT,
     code		VARCHAR(15),
 	FOREIGN KEY (nurse_id) REFERENCES nurse(nurse_id),
-	FOREIGN KEY (code) REFERENCES medication(name)
+	FOREIGN KEY (code) REFERENCES instruction(code)
 );
 
 CREATE TABLE medication_provision (
-	nurse_id       	 INT	PRIMARY KEY,
+	nurse_id       	 INT,
     name           	 VARCHAR(50),
 	FOREIGN KEY (nurse_id) REFERENCES nurse(nurse_id),
 	FOREIGN KEY (name) REFERENCES medication(name)
